db2cd97628aa774f0a9847f2b95cca9df79f51ab
rahat <md.amire02@gmail.com>
2021-08-17 16:29:59 +0600

    Created zip and zip_test until zip list

--- PythonZipModule/Test/test_zip.py
+++ PythonZipModule/Test/test_zip.py
@@ -10,22 +10,64 @@ class TestZipper(unittest.TestCase):
     def setUp(self) -> None:
         testdir = os.path.join(os.path.dirname(__file__), 'testdir')
         testfile = os.path.join(testdir, 'testfile.txt')
-        testzip = os.path.join(testdir, 'testzip.zip')
+        testfile2 = os.path.join(testdir, 'testfile2.txt')
         if not os.path.isdir(testdir):
             os.mkdir(testdir)
         with open(testfile, 'w') as tf:
             tf.write('This is a test file\n')
+        with open(testfile2, 'w') as tf:
+            tf.write('This is 2nd test file\n')
 
         self.testdir = testdir
-        self.testzip = testzip
         self.testfile = testfile
+        self.testfile2 = testfile2
 
     def test_add(self):
-        zipper = Zipper(self.testzip)
+        #test zip 1
+        testzip = os.path.join(self.testdir, 'testzip.zip')
+
+        zipper = Zipper(os.path.join(self.testdir, 'testzip'))
+        self.assertEqual(zipper.zipfile, testzip)
+
+
         zc = zipper.add(self.testfile, '/abcd/abcd.txt')
         self.assertEqual(isinstance(zc, ZipComplete), True)
         self.assertEqual(zc.get_returncode(), 0)
-        self.assertEqual(os.path.isfile(self.testzip), True)
+        self.assertEqual(os.path.isfile(testzip), True)
+
+        #Test zip 2
+        testzip2 = os.path.join(self.testdir, "subdir", 'testzip.zip')
+        zipper = Zipper(testzip2)
+        with self.assertRaises(OSError):
+            zipper.add(self.testfile)
+
+        zipper.add(self.testfile, createDir=True)
+        self.assertEqual(zipper.zipfile, testzip2)
+        self.assertEqual(os.path.isfile(testzip2), True)
+
+        #test update
+        zc = zipper.update(self.testfile2)
+        self.assertEqual(zc.get_returncode(), 0)
+
+        #test remove
+        self.assertEqual(zipper.remove('testfile2.txt').get_returncode(), 0)
+
+        #test extract
+        extractdir = os.path.join(self.testdir, 'outdir')
+        with self.assertRaises(OSError):
+            zipper.extract(extractdir)
+
+        zipper.extract(extractdir, createDir=True)
+        self.assertTrue(os.path.isfile(os.path.join(extractdir, 'testfile.txt')))
+
+        extractdir2 = os.path.join(self.testdir, 'outdir2')
+        with self.assertRaises(OSError):
+            zipper.extractWithPath(extractdir2)
+
+        zipper.extractWithPath(extractdir2, createDir=True)
+        self.assertTrue(os.path.isfile(os.path.join(extractdir2, 'testfile.txt')))
+
 
     def tearDown(self) -> None:
         shutil.rmtree(self.testdir)
+
--- PythonZipModule/util/zip.py
+++ PythonZipModule/util/zip.py
@@ -1,6 +1,6 @@
 from genericpath import isfile
 import os
-import sys
+import re
 
 from .exec7za import Exec7za
 from .zipcomplete import ZipComplete
@@ -8,31 +8,44 @@ from .ziplist import ZipList
 
 class Zipper():
 
-    def __init__(self, path) -> None:
-        self.zipfile = path
+    def __init__(self, path, *, password=None) -> None:
+        dir = os.path.dirname(path)
+        zip = os.path.basename(path)
+        if not re.search(r'\.zip$', zip):
+            zip = zip + ".zip"
 
-    def __dirCheck(self, outdir, create):
+        self.zipfile = os.path.join(dir, zip)
+        self.password = password
+
+
+    def _dirCheck(self, outdir, create):
         if os.path.isfile(outdir):
-            raise OSError("Specified out directory is a file")
+            raise OSError(f"Specified directory: {outdir} is a file")
 
         if not os.path.isdir(outdir):
             if create:
-                os.makedirs(outdir)
+                os.makedirs(outdir, mode=0o777)
             else:
                 raise OSError("Directory do not exist: {}".format(outdir))
 
-    def zipCheck(self) -> bool:
+
+    def _zipCheck(self) -> bool:
         if not os.path.isfile(self.zipfile):
             raise OSError(f'Zip file {self.zipfile} do not exist')
 
 
-    def add(self, *filelist) -> ZipComplete:
-        e7obj = Exec7za('a', self.zipfile)
+
+    def add(self, *filelist, createDir=False) -> ZipComplete:
+        self._dirCheck(os.path.dirname(self.zipfile), createDir)
 
         filelistfilter = [i for i in filelist if os.path.isfile(i)]
         
         if not len(filelistfilter):
             raise FileNotFoundError("Files do not exist. Could not create zip file")
+
+        e7obj = Exec7za('a', self.zipfile)
+        if self.password:
+            e7obj.add_args(f'-p{self.password}')
         
         e7obj.add_args(*filelistfilter)
         zipcomplete = e7obj.run()
@@ -41,18 +54,35 @@ class Zipper():
 
 
     def remove(self, *filelist) -> ZipComplete:
+        self._zipCheck()
+
         if not filelist:
             return ZipComplete(0, '', '')
 
         e7obj = Exec7za('d', self.zipfile)
+        if self.password:
+            e7obj.add_args(f'-p{self.password}')
+
         e7obj.add_args(*filelist, '-r')
 
         zipcomplete = e7obj.run()
         return zipcomplete
 
 
-    def update(self):
-        pass
+    def update(self, *filelist):
+        self._zipCheck()
+
+        if not filelist:
+            return ZipComplete(0, '', '')
+
+        e7obj = Exec7za('u', self.zipfile)
+        if self.password:
+            e7obj.add_args(f'-p{self.password}')
+
+        e7obj.add_args(*filelist)
+
+        zipcomplete = e7obj.run()
+        return zipcomplete
 
 
     def getList(self) -> ZipList:
@@ -61,15 +91,19 @@ class Zipper():
 
         return ZipList(zc.get_stdout())
 
-    def extract(self, outdir:str, *filelist, create:bool=False) -> ZipComplete:
+
+    def extract(self, outdir:str, *filelist, createDir:bool=False) -> ZipComplete:
         """
         filelist can be a glob or a file name
         """
-        self.__dirCheck(outdir, create)
+        self._zipCheck()
+        self._dirCheck(outdir, createDir)
 
         filelistfilter = filelist #create a filelist filter with regex
 
         e7obj = Exec7za('e', self.zipfile, f'-o{outdir}')
+        if self.password:
+            e7obj.add_args(f'-p{self.password}')
         if filelistfilter:
             e7obj.add_args(*filelistfilter)
         e7obj.add_args('-r')
@@ -77,14 +111,17 @@ class Zipper():
         zipcomplete = e7obj.run()
         return zipcomplete
 
-    def extractWithPath(self, outdir:str, *filelist, create:bool=False) -> ZipComplete:
+
+    def extractWithPath(self, outdir:str, *filelist, createDir:bool=False) -> ZipComplete:
         """
         filelist can be a glob or a file name
         """
-
-        self.__dirCheck(outdir, create)
+        self._zipCheck()
+        self._dirCheck(outdir, createDir)
 
         e7obj = Exec7za('x', self.zipfile, f'-o{outdir}')
+        if self.password:
+            e7obj.add_args(f'-p{self.password}')
         if filelist:
             e7obj.add_args(*filelist)
         e7obj.add_args('-r')
